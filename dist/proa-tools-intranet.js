6d4b3a10049b58c4d95fe3b94f57dfb7e3f8dba1
Miquel Alejandro Vicens Dalmau <mvicens@proadata.com>
2018-11-02 09:31:27 +0100

    Fix NPM package lock version
    And updated dependencies (also Bower's).

--- dist/proa-tools-intranet.js
+++ dist/proa-tools-intranet.js
@@ -1,5 +1,5 @@
 /*!
- * Proa Tools Intranet v1.3.4 (https://github.com/proa-data/proa-tools-intranet)
+ * Proa Tools Intranet v1.3.5 (https://github.com/proa-data/proa-tools-intranet)
  */
 
 ( function() {
