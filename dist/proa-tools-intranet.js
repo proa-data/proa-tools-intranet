41051da0702003c5c3d0a5304c7b797e0f79134d
Miquel Vicens <mvicens@proadata.com>
2020-04-02 14:26:11 +0200

    Update Proa Tools Records to fix

--- dist/proa-tools-intranet.js
+++ dist/proa-tools-intranet.js
@@ -1,5 +1,5 @@
 /*!
- * Proa Tools Intranet v2.5.0 (https://github.com/proa-data/proa-tools-intranet)
+ * Proa Tools Intranet v2.5.1 (https://github.com/proa-data/proa-tools-intranet)
  */
 
 ( function() {
