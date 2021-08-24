b821991ee20c3cee71e93a0bebbec3b3d6f3b80f
Miquel Alejandro Vicens Dalmau <mvicens@proadata.com>
2021-08-24 16:59:23 +0200

    Release patch version

--- dist/proa-tools-intranet.js
+++ dist/proa-tools-intranet.js
@@ -1,5 +1,5 @@
 /*!
- * Proa Tools Intranet v5.0.0 (https://github.com/proa-data/proa-tools-intranet)
+ * Proa Tools Intranet v5.0.1 (https://github.com/proa-data/proa-tools-intranet)
  */
 
 ( function() {
